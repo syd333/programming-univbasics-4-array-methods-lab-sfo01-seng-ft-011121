bec107acb4a0f430fcf56896f6fbc731420fffe2
sydney miranda <sydmiranda@icloud.com>
2020-12-19 16:05:17 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,15 +3,15 @@ array.include?(element)
 end
 
 def using_sort(array)
-
+array.sort
 end
 
 def using_reverse(array)
-
+array.reverse
 end
 
 def using_first(array)
-
+array.first
 end
 
 def using_last(array)
